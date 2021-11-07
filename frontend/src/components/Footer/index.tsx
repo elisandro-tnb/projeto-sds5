505ab33fc0124e2bf2ae69a9e57dab4fabd7ce33
Elisandro Rodrigues <ks2002br@yahoo.com.br>
2021-11-06 22:08:15 -0300

    url git fix 2

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,8 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/elisandro-tnb" target="_blank" rel="noreferrer">Elisandro Rodrigues Duarte</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/elisandro-tnb" target="_blank" rel="noreferrer">Elisandro Rodrigues Duarte  </a> <br />
+                Canal Youtube <a href="https://www.youtube.com/channel/UC9N00xmtikT8iJgci5wKSrg" target="_blank" rel="noreferrer">Youtube </a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
